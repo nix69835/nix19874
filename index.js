89bf7131be61ee95e19f5eee3d87c6d76d99289f
nix69835 <125588376+nix69835@users.noreply.github.com>
2024-02-14 23:21:17 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+        const daysEl = document.getElementById("days");
+ const hoursEl = document.getElementById("hours");
+ const minsEl = document.getElementById("mins");
+ const secEl = document.getElementById("sec"); 
+
+ const newYear = '17 mar 2024'; 
+
+  function countdown(){
+      const newYearDate = new Date(newYear);
+      const currentDate = new Date();
+
+      const totalSeconds = Math.abs((currentDate- newYearDate)/1000);
+
+      const days = Math.floor(totalSeconds/3600/24);
+      const hours = Math.floor(totalSeconds/3600) % 24;
+
+      const mins = Math.floor(totalSeconds/60) % 60;
+      const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML=days;
+     hoursEl.innerHTML=formatTime(hours);
+     minsEl.innerHTML= formatTime(mins);
+     secEl.innerHTML=formatTime(seconds);
+     
+  }
+  function formatTime(time){
+      if (time < 10) {
+          return ("0"+time)
+      } else {
+          return time
+      }
+  }
+
+  countdown();
+
+  setInterval(countdown, 1000);
